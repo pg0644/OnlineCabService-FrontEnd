f33ad713db2b7e1a4d034d9ce2ce31a7522edf89
pg0644 <PraneethReddyGunna@my.unt.edu>
2024-10-01 20:10:20 -0500

    login and registration components

---
+++ README.md
@@ -0,0 +1,14 @@
+## Online Cab Booking UI
+
+### Setup
+> npm install
+
+> npm start
+
+### Preview
+
+> Milestone 1
+
+![Login Page](/public/login.png)
+
+![Registration Page](/public/registration.png)
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <header className="jumbotron">
+        <h2>Welcome to our Online cab booking application</h2>
+        <h4>Please login or sign up!</h4>
+      </header>
+    </div>
+  );
+};
+
+export default Home;
